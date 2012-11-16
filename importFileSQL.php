8130fd177df70ecc6be17a0b4f2983ba235abe8f
Dane  Miller <dmille83@viper.cis.ksu.edu>
2012-11-15 19:51:22 -0600

    Updated addrecord gui. Added importFileSQL.php.

--- importFileSQL.php
+++ importFileSQL.php
@@ -1,24 +1,30 @@
 <?php
-  $filePath = $_GET["path"];
-  
-  $file = fopen($filePath, 'r');
-
+  // Connect to the database
   $conn = mysql_connect("mysql.cis.ksu.edu", "colecoop", "insecurepassword");
- 
-  if ($conn) { 
+  if ($conn) {
     mysql_select_db("colecoop", $conn);
 
-    if ($file) {
-      while (($data = fgetcsv($file, 1000, ",")) !== FALSE) {
-        mysql_query("INSERT INTO ag_counties (county, lat, lon) VALUES ('$data[0]', '$data[1]', '$data[2]')");
-      }
-      print "County data added successfully.";
+    $filePath = $_POST["inputFile"];
+  
+    $file = fopen($filePath, 'r');
+
+    $conn = mysql_connect("mysql.cis.ksu.edu", "colecoop", "insecurepassword");
+ 
+    if ($conn) { 
+      mysql_select_db("colecoop", $conn);
+
+      if ($file) {
+        while (($data = fgetcsv($file, 1000, ",")) !== FALSE) {
+//          mysql_query("INSERT INTO ag_counties (county, lat, lon) VALUES ('$data[0]', '$data[1]', '$data[2]')");
+        }
+        print "County data added successfully.";
+      } else {
+        print "Error opening file: $filePath";
+      } 
     } else {
-      print "Error opening file: $filePath";
-    } 
-  } else {
-    print "Error connecting to database!";
+      print "Error connecting to database!";
+    }
+    mysql_close($conn);
+    fclose($file);
   }
-  mysql_close($conn);
-  fclose($file);
 ?>
