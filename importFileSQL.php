187c84a7d3004b73992cfa2fac362355fa1b17e2
Blake  Koblitz <bkoblitz@viper.cis.ksu.edu>
2012-11-29 00:55:10 -0600

    Worked on import stuff

--- importFileSQL.php
+++ importFileSQL.php
@@ -1,35 +1,69 @@
 <?php
-
-//print_r($_FILES);
-$filePath = $_FILES["inputFile"]["name"];
-//print "File path: $filePath" . "<br>";
-$fileType = substr($filePath, -3);
-//print "Filetype: $fileType" . "<br>";
-if ($fileType != "csv"){
-  print "Error opening file: $filePath is wrong file type. Please verify that the file you are attempting to import is a .csv file.";
-} else {
-  print "Correct file type." . "<br>";
-
-  // Connect to the database
-  $conn = mysql_connect("mysql.cis.ksu.edu", "colecoop", "insecurepassword");
-  if ($conn) {
-    mysql_select_db("colecoop", $conn);
-
-    $file = fopen($filePath, 'r');
-    if ($file == TRUE) {
-      while (($data = fgetcsv($file, 1000, ",")) !== FALSE) {
-        echo "Year: " . $data[0] . " County: " . $data[1] . " MID: " . $data[2] . " Value: " . $data[3] . " Measurement: " . $data[4];
-        //mysql_query("INSERT INTO ag_counties (year, countyName, mid, value, measurement) VALUES ('$data[0]', '$data[1]', '$data[2]'), '$data[3]'), '$data[4]')");
-      }
-      //print "County data added successfully." . "<br>";
-    } else {
-      print "Error opening file: $filePath" . "<br>";
-    } 
-  } else {
-    print "Error connecting to database." . "<br>";
+$filePath = $_FILES["inputFile"]["tmp_name"];
+// Check to see if the file is of the right type
+if ($_FILES["inputFile"]["type"] != "application/vnd.ms-excel" && $_FILES["inputFile"]["type"] != "text/csv")
+  {
+  print "Error opening file: $filePath is wrong file type. Please verify that the file you are attempting to import is a .csv file." . "<br>";
   }
-  mysql_close($conn);
+  else
+  {
+    print "Correct file type." . "<br>";
+  } 
+  // Connect to the database
+  $mysqli = new mysqli("mysql.cis.ksu.edu", "colecoop", "insecurepassword", "colecoop");
+  $mysqli->autocommit(FALSE);
+  // Open the file or exit if the file cannot be opened
+  $file = fopen(($filePath), "r") or exit ("Can't open file.");
+  // Read the file and insert the data into the mysql database
+  while (($data = fgetcsv($file, 1000, ",")) != FALSE)
+  {
+     echo "Year: " . $data[0] . " County: " . $data[1] . " Value: " . $data[2]. " Measurement: " . $data[3] . "<br>";
+     
+     if ($result = $mysqli->query("SELECT mid FROM agcom_measurement WHERE measurement = '$data[3]'"))
+     {
+         $row = $result->fetch_row();
+         $mid = $row[0];
+     }
+
+     else
+     {
+         $query_ok=true; 
+         $mysqli->query("INSERT INTO agcom_measurement (measurement) VALUES ('$data[3]')") ? null : $query_ok=false;
+         if ($query_ok)
+         {
+            $mysqli->commit();
+         }
+
+         else 
+         {
+            $mysqli->rollback();
+            print "Unable to add measurement to the database." . "<br>";
+            continue;
+         }
+
+         $result = $mysqli->query("SELECT mid FROM agcom_measurement WHERE measurement = '$data[3]'");
+         $row = $result->fetch_row();
+         $mid = $row[0];
+     }
+     $query_ok=true;
+     $mysqli->query("INSERT INTO agcom_commodities (year, CountyName, type, mid, value) VALUES('$data[0]', '$data[1]', '$data[2]', $mid, '$data[4]')") ? null : $query_ok=false;
+     if ($query_ok)
+         {
+            $mysqli->commit();
+         }
+
+         else
+         {
+            $mysqli->rollback();
+            print "Unable to add row to the database." . "<br>";
+            continue;
+         }
+     print "County data added successfully." . "<br>";
+  }
+ 
+  // Close the file and close the connection to the database
+  $mysqli->close();
   fclose($file);
-}
+
 print '<br><a href="./index.php">Cancel</a>';
 ?>
